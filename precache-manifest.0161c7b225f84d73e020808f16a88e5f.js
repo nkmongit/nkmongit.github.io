09723226498347ac50904e3c48c2b12db88e8827
nkmongit <nnkm18@gmail.com>
2021-09-13 03:16:11 +0530

    Updates

---
+++ precache-manifest.0161c7b225f84d73e020808f16a88e5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c6b84c132e3e0b934251d2006bb080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d871bb3018847d721c9a",
+    "url": "/static/css/main.e16032d7.chunk.css"
+  },
+  {
+    "revision": "3b9857ad7af0e895262a",
+    "url": "/static/js/2.8851da78.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.8851da78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d871bb3018847d721c9a",
+    "url": "/static/js/main.1e9e1889.chunk.js"
+  },
+  {
+    "revision": "4d030bf2aa0c41b6f6cf",
+    "url": "/static/js/runtime-main.ccef853a.js"
+  },
+  {
+    "revision": "7c39a85da2f10ec885c93f43d89cb734",
+    "url": "/static/media/nkm.7c39a85d.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/resume.d41d8cd9.pdf"
+  }
+]);
